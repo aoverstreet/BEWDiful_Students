e8cb57bd7d5496e20bfc42deefcf6ed8ce81c08d
adam <adam.overstreet@gmail.com>
2013-06-19 20:23:45 -0400

    adding homework

--- 02_Variables_Conditionals/homework/adam_overstreet.rb
+++ 02_Variables_Conditionals/homework/adam_overstreet.rb
@@ -1,42 +1,33 @@
-def hello(name)
-  puts "Hello #{name}"
-
-
-name = gets
-
-secret_num = 5 
-tries = 3
-
-puts "Welcome, " +name+  "to the Secret Number game.  
-You have three tries to guess a secret number between 1 and 10. Please guess a number?" 
-
-users_guess = gets
-
-
-	if users_guess == secret_num
-		puts "Congrats, "+name+" you guessed the right number!" 
+def game 
+	puts "Hello, what is your name?"
+	name = gets
 	
-	else 
-		puts "Please try again, you have "+ tries - 1 + "remaining. Please guess again"  #first try
-			
-			users_guess = gets
+	puts "Welcome, #{name} to the Secret Number game.  
+	You have three tries to guess a secret number between 1 and 10. Please guess a number?" 
 	
-			if users_guess == secret_num
-				puts "Congrats, "+name+" you guessed the right number!" 
-			else		
-				puts "Please try again, you have "+ tries - 2 + "remaining. Please guess again"  #second try
-				
-				users_guess = gets
-				
-					if users_guess == secret_num
-						puts "Congrats, "+name+" you guessed the right number!" 
-					else		
-						puts "You have lost the game!" #third try
-					end
-				
+	users_guess = gets
+	
+	secret_number = 5
+	
+	if users_guess == secret_number
+		puts "Congrats, #{name} you guessed the right number!" 
+	else
+		puts "Please guess a number?"
+		
+		guesses =3
+		while guesses > -1
+		
+			if guesses > 0
+				puts "#{guesses} guesses remaining." 
+			else	
+				puts "you are out of guesses."
 			end
-	
+			
+			count -=1
+			
+		end
+		
 	end
 
-end
 
+end
---
+++ 02_Variables_Conditionals/homework/old_adam_overstreet.rb
@@ -0,0 +1,39 @@
+def game
+	puts "Hello, what is your name?"
+	name = gets
+	
+	secret_num = 5 
+	tries = 3
+	
+	puts "Welcome, " +name+  "to the Secret Number game.  
+	You have three tries to guess a secret number between 1 and 10. Please guess a number?" 
+	
+	users_guess = gets
+
+		if users_guess == secret_num
+			puts "Congrats, "+name+" you guessed the right number!" 
+		
+		else 
+			puts "Please try again, you have "+ tries - 1 + "remaining. Please guess again"  #first try
+			
+			users_guess = gets
+		
+			if users_guess == secret_num
+				puts "Congrats, "+name+" you guessed the right number!" 
+			else		
+			puts "Please try again, you have "+ tries - 2 + "remaining. Please guess again"  #second try
+			
+			users_guess = gets
+			
+				if users_guess == secret_num
+					puts "Congrats, "+name+" you guessed the right number!" 
+				else		
+					puts "You have lost the game!" #third try
+				end
+			
+			end
+		
+		end
+
+end
+
