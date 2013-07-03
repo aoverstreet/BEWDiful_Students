c4c21f3a96c757d92ca9adae91140f5e4259b9e1
adam <adam.overstreet@gmail.com>
2013-07-03 00:50:40 -0400

    adding files for lesson 7

---
+++ 07_My_First_Rails_App/code_alongs/books/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Books::Application.config.secret_key_base = 'feaeb74d722166363407d6674ff66fef0f47cab2341619c2e0dbcd7e89ad8bcd1951e7e15fe5cd4921a6f08471f46a639ae007e7179edbc1e8778d6b9602b662'
---
+++ 07_My_First_Rails_App/code_alongs/lesson_07.rb
@@ -0,0 +1,38 @@
+rails -v
+
+rails new books 
+
+rails server
+
+
+##scaffold
+rails generate scaffold Book title:string author:string year:integer
+
+##modification to the database  -- Migration
+
+
+Create 
+Read
+Update
+Destroy
+
+http verbs:
+http://sansari.github.io/BEWDiful_Students/08_routes_cont_views/slides/#/5
+
+MVC
+http://sansari.github.io/BEWDiful_Students/08_routes_cont_views/slides/#/7
+
+Model
+
+View
+
+Controller
+Do some work and render some stuff back.  
+Controllers are objects! 
+
+Methods are called actions in Rails
+
+
+### lost at 9:00pm, making a new controller called "games"
+
+
